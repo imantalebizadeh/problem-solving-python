8fd30356533520fd01bb968a879fb12045e73d81
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-03-09 12:53:29 +0330

    Initial commit

---
+++ problem-solving-01/main.py
@@ -0,0 +1,23 @@
+print(type(True))
+# <class 'bool'>
+
+print(type(False))
+# <class 'bool'>
+
+print(type(None))
+# <class 'NoneType'>
+
+print(type(1234))
+# <class 'int'>
+
+print(type(12.5))
+# <class 'float'>
+
+print(type("Iman"))
+# <class 'str'>
+
+print(type(int(3.1)))
+# <class 'int'>
+
+print(type(float(4)))
+# <class 'float'>
