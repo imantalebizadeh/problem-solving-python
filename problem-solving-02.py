813083967e2a7baab24655235a9c565dbaaa8aa3
Iman Talebizadeh <imantalebizadeh01@gmail.com>
2025-04-13 10:30:09 +0330

    Add basic arithmetic and comparison operations demonstration script

---
+++ problem-solving-02.py
@@ -0,0 +1,47 @@
+print(2==2)
+# True
+
+print(2!=3)
+# True
+
+print(2>3)
+# False
+
+print(2<3)
+# True
+
+print(2>=2)
+# True
+
+print(2<=2)
+# True
+
+print(2==2 and 3==3)
+# True 
+
+print(2==2 or 3==4)
+# True
+
+print(not 2==2)
+# False
+
+print(2*2)
+# 4
+
+print(2+2)
+# 4
+
+print(2-2)
+# 0
+
+print(2/2)
+# 1.0
+
+print(2//2)
+# 1
+
+print(2%2)
+# 0
+
+print(2**2)
+# 4
